77d94ea1595ae8cc2e24af8c887b1971c2e44c47
shwptjd <shwptjd1@gmail.com>
2021-08-30 15:22:15 +0900

    articles.html step 1

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
-
+from flask import Flask, render_template
+from data import Articles
 app = Flask(__name__)
 
 @app.route('/hello')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/data',methods=['GET','POST'])
+def index():
+    name = "KIM"
+    return render_template('index.html',data=name)
+
+@app.route('/articles',methods=['GET','POST'])
+def articles():
+    list_data = Articles()
+    return render_template('articles.html',data = list_data)
+
 if __name__ == '__main__':
-    app.run(port=8000 , debug=True)
+    app.run(debug=True)
